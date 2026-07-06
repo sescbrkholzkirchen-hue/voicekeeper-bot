a9349c7c97551862e8736faa16d1cc071487d204
sescbrkholzkirchen-hue <sescbrkholzkirchen@gmail.com>
2026-07-06 09:04:44 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,26 +8,29 @@ VOICE_CHANNEL_ID = int(os.getenv("VOICE_CHANNEL_ID"))
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
+voice_client = None
+
 @client.event
 async def on_ready():
+    global voice_client
     print(f"Eingeloggt als {client.user}")
-
-    channel = client.get_channel(VOICE_CHANNEL_ID)
-
-    if channel is None:
-        print("Voice-Channel nicht gefunden.")
-        return
 
     while True:
         try:
-            if not client.voice_clients:
-                await channel.connect()
-                print(f"Verbunden mit: {channel.name}")
+            channel = client.get_channel(VOICE_CHANNEL_ID)
 
-            await asyncio.sleep(60)
+            if channel is None:
+                print("Voice-Channel nicht gefunden.")
+            elif voice_client is None or not voice_client.is_connected():
+                voice_client = await channel.connect()
+                print(f"Verbunden mit: {channel.name}")
+            else:
+                print("Bot ist weiterhin verbunden.")
+
+            await asyncio.sleep(300)
 
         except Exception as e:
             print(f"Fehler: {e}")
-            await asyncio.sleep(10)
+            await asyncio.sleep(30)
 
 client.run(TOKEN)
